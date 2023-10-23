04806a175dfd73607efcf5141c35ed95170c0b3b
Notizou <noah.decroix3@gmail.com>
2023-10-23 14:43:19 +0200

    Add collection works

--- src/Form/CollectionType.php
+++ src/Form/CollectionType.php
@@ -3,17 +3,31 @@
 namespace App\Form;
 
 use App\Entity\Collection;
+use App\Repository\CategoryRepository;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CollectionType extends AbstractType
 {
+    public CategoryRepository $categoryRepository;
+    public function __construct (CategoryRepository $categoryRepository)
+    {
+        return$this->categoryRepository = $categoryRepository;
+    }
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('name')
-            ->add('category')
+            ->add('category', ChoiceType::class, [
+                'choices' => [
+                    'Jeux vidéos'  => $this->categoryRepository->find(1),
+                    'Jeux de sociétés'  => $this->categoryRepository->find(2),
+                    'Manga'  => $this->categoryRepository->find(3),
+                    'Films'  => $this->categoryRepository->find(4),
+                ]
+            ])
         ;
     }
 
