368d1fcd46563fe63ef38c41543aae48a14c8957
Notizou <noah.decroix3@gmail.com>
2023-10-23 14:48:14 +0200

    Add item works

--- src/Form/ItemType.php
+++ src/Form/ItemType.php
@@ -3,12 +3,20 @@
 namespace App\Form;
 
 use App\Entity\Item;
+use App\Repository\CategoryRepository;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ItemType extends AbstractType
 {
+    public CategoryRepository $categoryRepository;
+
+    public function __construct(CategoryRepository $categoryRepository)
+    {
+        return $this->categoryRepository = $categoryRepository;
+    }
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -19,8 +27,14 @@ class ItemType extends AbstractType
             ->add('platform')
             ->add('nbPlayer')
             ->add('maker')
-            ->add('borrow')
-            ->add('category')
+            ->add('category', ChoiceType::class, [
+                'choices' => [
+                    'Jeux vidéos'  => $this->categoryRepository->find(1),
+                    'Jeux de sociétés'  => $this->categoryRepository->find(2),
+                    'Manga'  => $this->categoryRepository->find(3),
+                    'Films'  => $this->categoryRepository->find(4),
+                ]
+            ])
         ;
     }
 
