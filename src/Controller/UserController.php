a84cd069ac34af5cdcfacecde59a2620beccfe79
Notizou <noah.decroix3@gmail.com>
2023-10-25 09:56:11 +0200

    add some secure

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -45,26 +45,32 @@ class UserController extends AbstractController
     #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
     public function show(User $user): Response
     {
-        return $this->render('user/show.html.twig', [
-            'user' => $user,
-        ]);
+        if ($this->getUser() === $user) {
+            return $this->render('user/show.html.twig', [
+                'user' => $user,
+            ]);
+        }
+        return $this->render('home/index.html.twig');
     }
 
     #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
+        if ($this->getUser() === $user) {
+            $form->handleRequest($request);
 
-            return $this->redirectToRoute('app_home', [], Response::HTTP_SEE_OTHER);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $entityManager->flush();
+
+                return $this->redirectToRoute('app_home', [], Response::HTTP_SEE_OTHER);
+            }
         }
 
         return $this->render('user/edit.html.twig', [
             'user' => $user,
-            'form' => $form,
+            'form' => $form->createView(),
         ]);
     }
 
